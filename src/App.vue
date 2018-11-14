7e501b0d8eff65bd7343cabfabdaeab0ab6db709
William Cresswell <william.cresswell5@gmail.com>
2018-11-14 21:57:12 +0000

    Add skeleton for detailed view and imported sqlite

--- src/App.vue
+++ src/App.vue
@@ -81,59 +81,65 @@
 </template>
 
 <script>
-  import Vue from 'vue'
-  export default {
-    data () {
-      return {
-        cordova: Vue.cordova,
-        title: 'Vuetify.js',
-        fab: false
+import Vue from "vue";
+export default {
+  data() {
+    return {
+      cordova: Vue.cordova,
+      title: "Vuetify.js",
+      fab: false,
+      db: null
+    };
+  },
+  created() {
+    var self = this;
+    this.cordova.on("deviceready", () => {
+      self.onDeviceReady();
+    });
+  },
+  methods: {
+    onDeviceReady: function() {
+      // Handle the device ready event.
+      this.cordova.on("pause", this.onPause, false);
+      this.cordova.on("resume", this.onResume, false);
+      if (this.cordova.device.platform === "Android") {
+        document.addEventListener("backbutton", this.onBackKeyDown, false);
       }
+      this.db = window.sqlitePlugin.openDatabase({
+        name: "my.db",
+        location: "default"
+      });
     },
-    created () {
-      var self = this
-      this.cordova.on('deviceready', () => {
-        self.onDeviceReady()
-      })
+    onPause() {
+      // Handle the pause lifecycle event.
+      console.log("pause");
     },
-    methods: {
-      onDeviceReady: function () {
-        // Handle the device ready event.
-        this.cordova.on('pause', this.onPause, false)
-        this.cordova.on('resume', this.onResume, false)
-        if (this.cordova.device.platform === 'Android') {
-          document.addEventListener('backbutton', this.onBackKeyDown, false)
-        }
-      },
-      onPause () {
-        // Handle the pause lifecycle event.
-        console.log('pause')
-      },
-      onResume () {
-        // Handle the resume lifecycle event.
-        // SetTimeout required for iOS.
-        setTimeout(function () {
-          console.log('resume')
-        }, 0)
-      },
-      onBackKeyDown () {
-        // Handle the back-button event on Android. By default it will exit the app.
-        navigator.app.exitApp()
-      }
+    onResume() {
+      // Handle the resume lifecycle event.
+      // SetTimeout required for iOS.
+      setTimeout(function() {
+        console.log("resume");
+      }, 0);
+    },
+    onBackKeyDown() {
+      // Handle the back-button event on Android. By default it will exit the app.
+      navigator.app.exitApp();
     }
   }
+};
 </script>
 
 <style>
-	body {
-    padding-top: constant(safe-area-inset-top);
-    padding-top: env(safe-area-inset-top);
-	}
-  .footer{ /* Apply this to v-bottom-nav if necessary. */
-    margin-bottom: constant(safe-area-inset-bottom);
-    margin-bottom: env(safe-area-inset-bottom);
-  }
-  #create .speed-dial {
+body {
+  padding-top: constant(safe-area-inset-top);
+  padding-top: env(safe-area-inset-top);
+}
+.footer {
+  /* Apply this to v-bottom-nav if necessary. */
+  margin-bottom: constant(safe-area-inset-bottom);
+  margin-bottom: env(safe-area-inset-bottom);
+}
+#create .speed-dial {
   position: absolute;
 }
 
--- src/components/Detailed.vue
+++ src/components/Detailed.vue
@@ -1,16 +1,128 @@
 <template>
     <v-card>
         <h1>Detailed View</h1>
+        <v-btn @click="loadDummyData()">loadDummyData</v-btn>
+        <v-data-table
+            :headers="headers"
+            :items="transactions"
+            class="elevation-1"
+            xs12
+            >
+            <template slot="items" slot-scope="props">
+                <tr @click="editTransaction(props.item)">
+                    <td>{{ props.item.dateTime }}</td>
+                    <td>{{ props.item.name }}</td>
+                    <td>{{ props.item.amount }}</td>
+                </tr>
+            </template>
+            <template slot="no-data">
+                <v-alert :value="true" color="error" icon="warning">
+                    Sorry, nothing to display here :(
+                </v-alert>
+            </template>
+        </v-data-table>
+        <v-dialog v-model="dialog">
+            <v-card>
+                <v-card-title>
+                    <span class="headline">Detailed Transaction</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container grid-list-md>
+                        <v-layout wrap>
+                            <v-flex xs12 sm6 md4>
+                                <v-text-field v-model="singleTransaction.name" label="Transaction name"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                                <v-text-field v-model="singleTransaction.amount" label="Amount"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                                <v-text-field v-model="singleTransaction.catagory" label="Catagory"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                                <v-text-field v-model="singleTransaction.dateTime" label="Date/Time"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                                <v-text-field v-model="singleTransaction.location" label="Location"></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+                    <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-card>
 </template>
 <script>
 export default {
-    data: function() {
-        return {
-            imageSrc: null
-        }
+  data: function() {
+    return {
+      headers: [
+        {
+          text: "Transaction Date",
+          value: "dateTime"
+        },
+        { text: "Name", value: "name" },
+        { text: "Amount", value: "amount" }
+      ],
+      transactions: [],
+      dialog: false,
+      singleTransaction: {},
+      selectedIndex: -1
+    };
+  },
+  created: function() {
+    this.transactions = this.getTransactions();
+  },
+  methods: {
+    getTransactions: function() {
+      let transactions = JSON.parse(
+        window.localStorage.getItem("transactions")
+      );
+      if (transactions == null) {
+        return [];
+      } else {
+        return transactions;
+      }
     },
-    methods: {
+    editTransaction: function(item) {
+      this.dialog = true;
+      this.singleTransaction = item;
+      this.selectedIndex = this.transactions.indexOf(item);
+    },
+    deleteTransaction: function(item) {
+      this.transactions.splice(this.selectedIndex, 1);
+      window.localStorage.setItem(
+        "transactions",
+        JSON.stringify(this.transactions)
+      );
+    },
+    close: function() {
+      this.dialog = false;
+      this.singleTransaction = {};
+    },
+    save: function() {
+        this.transactions[this.selectedIndex] = this.singleTransaction;
+        window.localStorage.setItem(
+        "transactions",
+        JSON.stringify(this.transactions)
+      );
+      this.dialog = false;
+    },
+    loadDummyData: function() {
+      let data = {
+        name: "tran 1",
+        amount: 10,
+        catagory: "shop",
+        dateTime: "today",
+        location: "that place"
+      };
+      window.localStorage.setItem("transactions", JSON.stringify([data]));
     }
-}
+  }
+};
 </script>
